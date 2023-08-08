5f5103268d2635f759c574cd8580543c91c7f205
YoussefAbdelmonem <elmatdor.yosef@gmail.com>
2023-08-08 11:30:19 +0300

    component

---
+++ lib/presentation/components/button_widget.dart
@@ -0,0 +1,67 @@
+import 'package:deraya_application/core/constant/colors.dart';
+import 'package:deraya_application/presentation/components/text_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+
+class ButtonWidget extends StatelessWidget {
+  String title;
+
+  double width, height, radius;
+  double? fontSize;
+  Widget? child;
+  Color textColor, buttonColor, borderColor, inkColor;
+  void Function()? onTap;
+  bool withBorder;
+
+  ButtonWidget(
+      {Key? key,
+        this.title = "OK",
+        this.width = 88.0,
+        this.height = 50.0,
+        this.onTap,
+        this.child,
+        this.fontSize,
+        this.textColor = Colors.white,
+        this.buttonColor = ColorsManger.primary,
+        this.borderColor = ColorsManger.primary,
+        this.inkColor = ColorsManger.primary,
+        this.withBorder = false,
+        this.radius = 80.0})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(buttonColor),
+          shadowColor: MaterialStateProperty.all<Color>(Colors.transparent),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(radius),
+                  side: withBorder
+                      ? BorderSide(color: borderColor)
+                      : const BorderSide(color: Colors.transparent)))),
+      onPressed: onTap,
+      child: Ink(
+        decoration: BoxDecoration(
+          color: inkColor,
+          borderRadius: BorderRadius.all(Radius.circular(radius)),
+        ),
+        child: child ??
+            Container(
+              constraints: BoxConstraints(minWidth: width, minHeight: height),
+              // min sizes for Material buttons
+              alignment: Alignment.center,
+              child: TextWidget(
+                title: title,
+                fontSize: fontSize ?? 16.sp,
+                textAlign: TextAlign.center,
+                color: textColor,
+              ),
+            ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/components/text_form_field.dart
@@ -0,0 +1,145 @@
+import 'package:deraya_application/core/constant/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+class TextFormFieldWidget extends StatefulWidget {
+  final String? hintText, label, errorText;
+  final TextInputType type;
+  bool password;
+  final bool expanded;
+  Color activeBorderColor, borderColor, backgroundColor;
+  bool floatingHint;
+  int? maxLines;
+  int? minLines;
+  void Function()? onTap;
+  FocusNode? focusNode;
+  TextAlign textalign;
+  int? maxLengh;
+  TextDirection? textdirection;
+  EdgeInsetsDirectional? contentPadding;
+  double borderRadius;
+  Widget? prefixIcon, suffixIcon, suffixWidget;
+  TextEditingController? controller;
+  InputDecoration? inputDecoration;
+  ValueChanged<String>? onChanged;
+  ValueChanged<String?>? onSaved;
+  String? Function(String?)? validate;
+  bool? enable;
+  bool? readOnly;
+  TextFormFieldWidget(
+      {required this.onChanged,
+        this.onSaved,
+        this.readOnly,
+        this.enable,
+        this.validate,
+        this.onTap,
+        this.password = false,
+        this.expanded = false,
+        this.floatingHint = false,
+        this.type = TextInputType.text,
+        this.hintText = "",
+        this.label,
+        this.textalign = TextAlign.start,
+        this.maxLengh,
+        this.errorText,
+        this.controller,
+        this.activeBorderColor = ColorsManger.primary,
+        this.borderRadius = 8.0,
+        this.borderColor = ColorsManger.primary,
+        this.backgroundColor = Colors.white,
+        this.maxLines,
+        this.minLines = 1,
+        this.prefixIcon,
+        this.suffixIcon,
+        this.suffixWidget,
+        this.inputDecoration,
+        this.contentPadding,
+        this.textdirection,
+        Key? key})
+      : super(key: key);
+
+  @override
+  State<TextFormFieldWidget> createState() => _TextFormFieldWidgetState();
+}
+
+class _TextFormFieldWidgetState extends State<TextFormFieldWidget> {
+  bool? isPass;
+  @override
+  void initState() {
+    isPass = widget.password ? true : false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      cursorColor: ColorsManger.primary,
+      readOnly: widget.readOnly ?? false,
+      enabled: widget.enable ?? true,
+      validator: widget.validate,
+      maxLength: widget.maxLengh,
+      focusNode: widget.focusNode,
+
+      controller: widget.controller,
+      maxLines: widget.maxLines ?? 1,
+      minLines: widget.minLines,
+      // autofocus: false,
+      // expands: widget.expanded,
+      textAlign: widget.textalign,
+      textDirection: widget.textdirection,
+      style: TextStyle(
+          color: ColorsManger.blackColor,
+          fontSize: 14.sp,
+          fontFamily: "cairo",
+          fontWeight: FontWeight.w600),
+      decoration: widget.inputDecoration ??
+          InputDecoration(
+            // isDense: true,
+              contentPadding: widget.contentPadding,
+
+              filled: true,
+              fillColor: widget.backgroundColor,
+              prefixIcon: widget.prefixIcon,
+              suffix: widget.suffixWidget,
+              suffixIcon: widget.suffixIcon ??
+                  (isPass!
+                      ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        widget.password = !widget.password;
+                      });
+                    },
+                    icon: Icon(
+                      widget.password
+                          ? Icons.remove_red_eye_outlined
+                          : Icons.visibility_off,
+                      color: ColorsManger.blackColor,
+                    ),
+                  )
+                      : widget.suffixIcon),
+              errorText: widget.errorText,
+              labelText: widget.label,
+              //label: TextWidget(widget.label),
+              focusedBorder: OutlineInputBorder(
+                  borderSide:
+                  BorderSide(color: widget.activeBorderColor, width: 1.0),
+                  borderRadius:
+                  BorderRadius.all(Radius.circular(60.r))),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 1.0),
+                  borderRadius:
+                  BorderRadius.all(Radius.circular(60.r))),
+              labelStyle: const TextStyle(color: ColorsManger.blackColor,fontFamily: "cairo",fontWeight: FontWeight.w400),
+              hintStyle: TextStyle(color: ColorsManger.blackColor, fontSize: 12.sp,fontFamily: "cairo",fontWeight: FontWeight.w400),
+              hintText: widget.hintText),
+      keyboardType: widget.type,
+      obscureText: widget.password,
+      onChanged: widget.onChanged,
+      onSaved: widget.onSaved,
+      onTap: () {
+        widget.onTap?.call();
+      },
+    );
+  }
+}
