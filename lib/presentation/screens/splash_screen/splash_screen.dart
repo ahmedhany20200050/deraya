e7dd1964f6c64e72a283839203db8c6eb618a07c
ahmed <ahmedhany20200050@gmail.com>
2023-08-20 20:10:00 +0300

    finished loading screen

--- lib/presentation/screens/splash_screen/splash_screen.dart
+++ lib/presentation/screens/splash_screen/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:deraya_application/presentation/screens/loading_screen/loading_screen.dart';
 import 'package:deraya_application/presentation/screens/splash_screen/second_splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +8,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Future.delayed(Duration(seconds: 3)).whenComplete((){
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => SecondSplashScreen()));
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoadingScreen()));
     });
     return WillPopScope(
       onWillPop: ()async=>false,
