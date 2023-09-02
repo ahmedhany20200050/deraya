d7eddb6182a5eb818f633d7cd0784d7fad80bef5
ahmed <ahmedhany20200050@gmail.com>
2023-09-02 20:05:37 +0300

    finish app

--- lib/presentation/screens/course_details/course_details_components/contant_tab_view.dart
+++ lib/presentation/screens/course_details/course_details_components/contant_tab_view.dart
@@ -1,11 +1,56 @@
+import 'dart:convert';
+
 import 'package:deraya_application/core/Utils/utils.dart';
 import 'package:deraya_application/core/constant/colors.dart';
+import 'package:deraya_application/domain/models/courses/courses_model.dart';
 import 'package:deraya_application/presentation/components/text_widget.dart';
+import 'package:deraya_application/presentation/screens/course_details/course_details_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:http/http.dart' as http;
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+class ContentTabView extends StatefulWidget {
+  late Courses course;
+   ContentTabView(Courses course, {super.key}){
+     this.course=course;
+   }
 
-class ContentTabView extends StatelessWidget {
-  const ContentTabView({super.key});
+  @override
+  State<ContentTabView> createState() => _ContentTabViewState(this.course);
+}
+
+class _ContentTabViewState extends State<ContentTabView> {
+  Courses course;
+  _ContentTabViewState(this.course);
+  List<String>videosLinks=['https://youtu.be/6QAELgirvjs?si=8YK-c-dvjIL1c0Uj'];
+  Future<void> getVideos()async {
+    http.Response response=await http.get(Uri.parse("https://diraya.xyz/api/video/course-video/${course.id}"));
+    if(response.statusCode==200){
+      setState(() {
+        dynamic json = jsonDecode(response.body)["data"];
+        if (json["video"] != null) {
+          for(var link in json["video"]){
+            videosLinks.add(link["video"]);
+          }
+        }
+        controller=YoutubePlayerController(
+          initialVideoId: YoutubePlayer.convertUrlToId(videosLinks[0])??"",
+          flags: YoutubePlayerFlags(
+            autoPlay: false,
+          ),
+        );
+      });
+      currentVideoLink=videosLinks[0];
+    }else{
+      print("videos errorr ya ahmed");
+    }
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getVideos();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +65,7 @@ class ContentTabView extends StatelessWidget {
             ),
             6.pw,
             TextWidget(
-              title: "15 موضوع ",
+              title: "${videosLinks.length} موضوع ",
               fontSize: 16.sp,
               color: AppColors.brownColor,
             ),
@@ -28,47 +73,54 @@ class ContentTabView extends StatelessWidget {
         ),
         ListView.builder(
           itemBuilder: (_, index) {
-            return Padding(
-              padding:  EdgeInsets.all(16.0.w),
-              child: Container(
-                color: Colors.grey.shade100,
-                child: Row(
-                  children: [
-                    Image.network(
-                      "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
-                      fit: BoxFit.cover,
-                      width: 80.w,
-                      height: 80.h,
-                    ),
-                    8.pw,
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextWidget(
-                          title: "مقدمة عن برنامج Adobe Photoshop",
-                          fontSize: 16.sp,
+            return InkWell(
+              onTap: (){
+                currentVideoLink=videosLinks[index];
+              },
+              child: Padding(
+                padding:  EdgeInsets.all(16.0.w),
+                child: Container(
+                  color: Colors.grey.shade100,
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Icon(
+                          Icons.play_circle_outlined,
                           color: AppColors.primary,
-
                         ),
-                        16.ph,
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Icon(Icons.replay_30,color: AppColors.grayColor,),
-                            TextWidget(title: "22:30",color: AppColors.grayColor,fontSize: 14.sp,),
-                           160.pw,
-                            TextWidget(title: "فيديو مجاني",color: AppColors.brownColor,fontSize: 14.sp,),
+                        width: 80.w,
+                        height: 80.h,
+                      ),
+                      8.pw,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextWidget(
+                            title: "مقدمة عن برنامج Adobe Photoshop",
+                            fontSize: 16.sp,
+                            color: AppColors.primary,
 
-                          ],
-                        )
-                      ],
-                    )
-                  ],
+                          ),
+                          16.ph,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              // Icon(Icons.replay_30,color: AppColors.grayColor,),
+                              TextWidget(title: "22:30",color: AppColors.grayColor,fontSize: 14.sp,),
+                             160.pw,
+                              TextWidget(title: "فيديو مجاني",color: AppColors.brownColor,fontSize: 14.sp,),
+
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             );
           },
-          itemCount: 6,
+          itemCount:videosLinks.length,
           shrinkWrap: true,
           physics: BouncingScrollPhysics(),
         ),
--- lib/presentation/screens/course_details/course_details_screen.dart
+++ lib/presentation/screens/course_details/course_details_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:autoscale_tabbarview/autoscale_tabbarview.dart';
 import 'package:deraya_application/core/Utils/utils.dart';
 import 'package:deraya_application/core/constant/colors.dart';
@@ -9,16 +11,25 @@ import 'package:deraya_application/presentation/screens/course_details/course_de
 import 'package:deraya_application/presentation/screens/course_details/course_details_components/comments_and_rating_view.dart';
 import 'package:deraya_application/presentation/screens/course_details/course_details_components/contant_tab_view.dart';
 import 'package:deraya_application/presentation/screens/home/widget/price_widget.dart';
+import 'package:deraya_application/presentation/screens/login/login_screen.dart';
 import 'package:deraya_application/presentation/screens/payments/payment_screen.dart';
 import 'package:deraya_application/presentation/widgets/video_player_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:http/http.dart' as http;
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 import 'course_details_components/courses_tab_bar.dart';
 import 'course_details_components/summary_tab_view.dart';
-
-class CourseDetailsScreen extends StatelessWidget {
-
+String? currentVideoLink;
+YoutubePlayerController controller = YoutubePlayerController(
+  initialVideoId: YoutubePlayer.convertUrlToId(currentVideoLink??" ")??"",
+  flags: YoutubePlayerFlags(
+    autoPlay: false,
+  ),
+);
+bool ok=false;
+class CourseDetailsScreen extends StatefulWidget {
   List<String> requirments = [
     // "لا يلزم على الطالب وجود خبرة سابقة في مجال التصميم ،فإن الدورة سوف تساعدك على التدريب باستمرار.",
     // "يلزم جهاز كمبيوتر أو نظام تشغيل مناسب للتعامل الجيد مع برنامج Adobe Photoshop في الدورة التدريبية.",
@@ -43,9 +54,93 @@ class CourseDetailsScreen extends StatelessWidget {
         requirments.add(c.requirementAr!);
       }
     }
+    controller = YoutubePlayerController(
+      initialVideoId: YoutubePlayer.convertUrlToId(currentVideoLink??" ")??"",
+      flags: YoutubePlayerFlags(
+        autoPlay: false,
+      ),
+    );
   }
 final bool paidSuccessfully;
 
+  @override
+  State<CourseDetailsScreen> createState() => _CourseDetailsScreenState(data: this.data,id: this.id,paidSuccessfully: this.paidSuccessfully,course: this.course,requirments: this.requirments);
+}
+
+class _CourseDetailsScreenState extends State<CourseDetailsScreen> {
+
+  List<String> requirments = [
+    // "لا يلزم على الطالب وجود خبرة سابقة في مجال التصميم ،فإن الدورة سوف تساعدك على التدريب باستمرار.",
+    // "يلزم جهاز كمبيوتر أو نظام تشغيل مناسب للتعامل الجيد مع برنامج Adobe Photoshop في الدورة التدريبية.",
+    // "يستحسن استخدام جهاز التحكم ( Mouse ) ، وذلك للتحكم الجيد مع البرنامج.",
+  ];
+  static const List<String> skills = [
+    // "الأدوات في فوتوشوب",
+    // 'تدرج الألوان',
+    // "دمج الصور",
+    'الأشكال',
+    "النصوص المختلفة",
+    "مهارات عامة ", // "المسافات وعمل تأثيرات",
+    // "بطاطس وجمبها رز"
+  ];
+  int? id;
+  List<Courses> data=[];
+  late Courses course;
+  _CourseDetailsScreenState({this.id, required this.data,this.paidSuccessfully=false,required this.requirments,required this.course}){
+    for(var c in data){
+      if(c.id==this.id){
+        course=c;
+        requirments.add(c.requirementAr!);
+      }
+    }
+  }
+  final bool paidSuccessfully;
+
+  List<String>videosLinks=[];
+  Future<void> getVideos()async {
+    http.Response response=await http.get(
+        Uri.parse("https://diraya.xyz/api/video/course-video/${course.id}"),
+      headers: <String, String>{
+        'Accept-Language': 'en',
+        "Accept": "application/json",
+        'Authorization': userUltraProMax?.token??"",
+      },
+    );
+    if(response.statusCode==200){
+
+      setState(() {
+        ok=false;
+        videosLinks.clear();
+      });
+      setState(() {
+        dynamic json = jsonDecode(response.body)["data"];
+        if (json["video"] != null) {
+          for(var link in json["video"]){
+            videosLinks.add(link["video"]);
+          }
+        }
+
+        // controller=YoutubePlayerController(
+        //   initialVideoId: YoutubePlayer.convertUrlToId(videosLinks[0])??"",
+        //   flags: YoutubePlayerFlags(
+        //     autoPlay: false,
+        //   ),
+        // );
+      });
+      setState(() {
+        ok=true;
+      });
+      controller.load(YoutubePlayer.convertUrlToId(videosLinks[0])??"");
+    }else{
+      print("videos errorr ya ahmed");
+    }
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getVideos();
+  }
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -67,10 +162,24 @@ final bool paidSuccessfully;
           child: Column(
             children: [
               16.ph,
-              VideoReview(
-                url:
-                    course.video??"https://assets.mixkit.co/videos/preview/mixkit-photograper-editing-his-work-6439-large.mp4",
-              ),
+              ok?YoutubePlayer(
+                onReady: (){
+
+                },
+                controller: controller,
+                showVideoProgressIndicator: true,
+                progressIndicatorColor: AppColors.primary,
+                bottomActions: [
+                  CurrentPosition(),
+                  ProgressBar(
+                    isExpanded: true,
+                    colors: ProgressBarColors(
+                      playedColor: AppColors.primary,
+                      backgroundColor: AppColors.lightBackground,
+                    ),
+                  ),
+                ],
+              ):Container(),
               // 16.ph,
               Padding(
                 padding: EdgeInsets.all(16.0.w),
@@ -78,7 +187,7 @@ final bool paidSuccessfully;
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     TextWidget(
-                      title:course.nameAr,
+                      title:widget.course.nameAr,
                       color: AppColors.primary,
                       fontSize: 26.sp,
                       fontWeight: FontWeight.w600,
@@ -93,7 +202,7 @@ final bool paidSuccessfully;
                     ),
                     16.ph,
                     TextWidget(
-                      title:course.descriptionAr,
+                      title:widget.course.descriptionAr,
                       color: AppColors.primary,
                       fontSize: 18.sp,
                       fontWeight: FontWeight.w600,
@@ -191,8 +300,116 @@ final bool paidSuccessfully;
                     ),
                     AutoScaleTabBarView(
                       children: [
-                      SummaryTabView(skills: skills, requirments: requirments,course:course),
-                        ContentTabView(),
+                      SummaryTabView(skills: CourseDetailsScreen.skills, requirments: requirments,course:widget.course),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.play_circle_outlined,
+                          color: AppColors.brownColor,
+                        ),
+                        6.pw,
+                        TextWidget(
+                          title: "${videosLinks.length} موضوع ",
+                          fontSize: 16.sp,
+                          color: AppColors.brownColor,
+                        ),
+                      ],
+                    ),
+                    ListView.builder(
+                      itemBuilder: (_, index) {
+                        return InkWell(
+                          onTap: (){
+                            if(index<2){
+                              // controller.dispose();
+                              controller.load(YoutubePlayer.convertUrlToId(videosLinks[index])??"");
+                              // setState(() {
+                              //   ok=false;
+                              // });
+                              // setState(() {
+                              //   controller=YoutubePlayerController(
+                              //     initialVideoId: YoutubePlayer.convertUrlToId(videosLinks[index])??"",
+                              //     flags: YoutubePlayerFlags(
+                              //       autoPlay: false,
+                              //     ),
+                              //   );
+                              // });
+                              // setState(() {
+                              //   ok=true;
+                              // });
+                            }else{
+                              if(paidSuccessfully){
+
+                                controller.load(YoutubePlayer.convertUrlToId(videosLinks[index])??"");
+                                // setState(() {
+                                //   ok=false;
+                                // });
+                                // setState(() {
+                                //   YoutubePlayerController(
+                                //     initialVideoId: YoutubePlayer.convertUrlToId(videosLinks[index])??"",
+                                //     flags: YoutubePlayerFlags(
+                                //       autoPlay: false,
+                                //     ),
+                                //   );
+                                // });
+                                // setState(() {
+                                //   ok=true;
+                                // });
+                              }
+                            }
+
+                          },
+                          child: Padding(
+                            padding:  EdgeInsets.all(16.0.w),
+                            child: Container(
+                              color: Colors.grey.shade100,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    child: Icon(
+                                      Icons.play_circle_outlined,
+                                      color: AppColors.primary,
+                                    ),
+                                    width: 80.w,
+                                    height: 80.h,
+                                  ),
+                                  8.pw,
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      TextWidget(
+                                        title: "فيديو ${index+1} ",
+                                        fontSize: 16.sp,
+                                        color: AppColors.primary,
+
+                                      ),
+                                      16.ph,
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+                                          // Icon(Icons.replay_30,color: AppColors.grayColor,),
+                                          TextWidget(title: "22:30",color: AppColors.grayColor,fontSize: 14.sp,),
+                                          160.pw,
+                                          TextWidget(title: index<2?"فيديو مجاني":"فيديو مدفوع",color: AppColors.brownColor,fontSize: 14.sp,),
+
+                                        ],
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      itemCount:videosLinks.length,
+                      shrinkWrap: true,
+                      physics: BouncingScrollPhysics(),
+                    ),
+                  ],
+                ),
                       CommentsAndRatingView(),
                     ],),
 
@@ -206,7 +423,7 @@ final bool paidSuccessfully;
             ],
           ),
         ),
-        bottomNavigationBar:paidSuccessfully?null: Container(
+        bottomNavigationBar:widget.paidSuccessfully?null: Container(
           height: 100.h,
           color: Colors.white,
           child: Padding(
@@ -216,7 +433,7 @@ final bool paidSuccessfully;
                 ButtonWidget(
                   width: 0.4.sw,
                   onTap: () {
-                    Utils.openScreen(context,  PaymentScreen(courseId: course.id!,));
+                    Utils.openScreen(context,  PaymentScreen(courseId: widget.course.id!,));
                   },
                   radius: 12.r,
                   title: "شراء الكورس",
@@ -242,7 +459,7 @@ final bool paidSuccessfully;
                               horizontal: 10.w, vertical: 4.w),
                           child: Center(
                             child: TextWidget(
-                              title: "E£${course.price}",
+                              title: "E£${widget.course.price}",
                               maxLines: 2,
                               textAlign: TextAlign.center,
                               fontSize: 16.sp,
@@ -254,7 +471,7 @@ final bool paidSuccessfully;
                     ),
                     8.ph,
                     TextWidget(
-                      title: "E£${double.parse(course.price??"1")+100}",
+                      title: "E£${double.parse(widget.course.price??"1")+100}",
                       maxLines: 2,
                       isOffer: true,
                       textAlign: TextAlign.center,
